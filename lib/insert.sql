0bf359876be405dbeb3b3cd3c401be33733c82fd
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-14 06:29:20 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 4, "male", "brown", "chill", "yes");
+VALUES ("Tabitha", 2, "female", "brown", "emotional", "yes");
+VALUES ("Sergeant Brown", 8, "male", "brown", "wild", "no");
+VALUES ("Melissa", 6, "female", "white", "rowdy", "yes");
+VALUES ("Grinch", 5, "male", "black", "fiesty", "no");
+VALUES ("Wendy", 1, "female", "black", "shy", "yes");
