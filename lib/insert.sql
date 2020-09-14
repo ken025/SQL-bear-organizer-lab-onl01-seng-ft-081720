226f20591e6fe20b64b83b451d40f8aecb875ec6
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-14 06:31:30 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,6 +2,9 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Mr. Chocolate", 4, "male", "brown", "chill", "yes");
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 6, "female", "white", "lazy", "yes")
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Tabitha", 2, "female", "brown", "emotional", "yes");
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
@@ -15,3 +18,6 @@ VALUES ("Grinch", 5, "male", "black", "fiesty", "no");
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Wendy", 1, "female", "black", "shy", "yes");
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 9, "female", "brown", NULL, "yes")
